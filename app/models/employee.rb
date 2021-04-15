019d219d481e4364165e11bdbf2e8f4f68fe4e98
Karl Heitmann <khf1988@gmail.com>
2021-04-15 17:49:16 -0400

    method search_sub_employees_level created, but it has some bugs

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,4 +7,16 @@ class Employee < ApplicationRecord
   has_many :employees, class_name: "Employee", foreign_key: "boss_id"
   belongs_to :boss, class_name: "Employee", optional:true
 
+  def search_sub_employees_level(level)
+    # puts "level: #{level}"
+    puts "I'm #{self.name} - level: #{level}"
+    if (level > 0)
+      self.employees.each {|employee| puts employee.name}
+      return self.employees + self.employees.map {|employee| employee.search_sub_employees_level(level - 1)}
+    else
+      self.employees.each {|employee| puts employee.name}
+      return self.employees
+    end
+  end
+
 end
