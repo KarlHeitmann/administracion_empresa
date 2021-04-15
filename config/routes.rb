2b4525e518d407437e239f1762244ca10ca120ad
Karl Heitmann <khf1988@gmail.com>
2021-04-14 21:32:04 -0400

    agregado controlador employees

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'employees/index'
   devise_for :employees
+
+  root 'employees#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
