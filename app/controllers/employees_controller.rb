6bc94264469ddf49269674228981420246b1c442
Karl Heitmann <khf1988@gmail.com>
2021-04-15 02:05:28 -0400

    add_boss button working

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,21 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, except: [:index]
   def index
     @employees = Employee.all
     @employee = Employee.new
   end
+
+  def show
+  end
+
+  def add_boss
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+    def set_employee
+      @employee = Employee.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'employees/index'
   devise_for :employees
 
+  resources :employees, only: [:index, :show]
+  post 'employees_boss/:id', to: 'employees#add_boss', as: 'employee_boss'
   root 'employees#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
