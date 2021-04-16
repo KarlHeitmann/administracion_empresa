59a486265d6c0ee6c1b276955a21f7b5cfd07ac0
Karl Heitmann <khf1988@gmail.com>
2021-04-16 00:10:34 -0400

    ver empleados casi funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'employees/index'
   devise_for :employees
+  get 'sub_employees/:id', to: 'employees#sub_employees', as: 'sub_employees'
 
   root 'employees#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
