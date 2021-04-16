1dd46f245a3b593fdcd41d8ed51baed7d909f2a2
Karl Heitmann <khf1988@gmail.com>
2021-04-16 00:10:34 -0400

    ver empleados casi funcionando

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,23 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, except: [:index]
   def index
-    @employees = Employee.all
+    @employees = Employee.all.order(created_at: :asc)
   end
+
+  def sub_employees
+    puts "########################"
+    puts "########################"
+    puts "########################"
+    puts "########################"
+    puts @employee.email
+    @sub_employees = @employee.employees
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+    def set_employee
+      @employee = Employee.find(params[:id])
+    end
 end
